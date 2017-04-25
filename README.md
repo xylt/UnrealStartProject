e2412c2d04d176adcd13516da483fe7eb3064ca6
xylt <782448785@qq.com>
2017-04-25 19:49:16 +0800

    修改忽略规则

---
+++ README.md
@@ -0,0 +1,2 @@
+# Unreal第一个项目
+使用UnReal4编写的QuickStart项目
---
+++ Source/MyQuickStart/MyQuickStart.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "MyQuickStart.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, MyQuickStart, "MyQuickStart" );
